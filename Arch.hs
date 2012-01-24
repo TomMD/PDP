29f038c58dc1b40643d9923df76f7340f8aebc85
Thomas M. DuBuisson <Thomas.DuBuisson@gmail.com>
2012-01-23 16:21:06 -0800

    Fix cycle counting (except for IO)s

--- Arch.hs
+++ Arch.hs
@@ -1,4 +1,9 @@
-module Arch where
+module Arch
+  ( nrCycles
+  ) where
+
+import Numeric (readOct)
+import Data.Char (isOctDigit)
 
 nrCyclesOp :: PDPOp -> Int
 nrCyclesOp o
@@ -10,4 +15,21 @@ nrCyclesOp o
  op = fromEnum o
 
 nrCycles :: Instr -> Int
-nrCycles = error "TODO"
+nrCycles instr@(Instr op _ _ _ _)
+  | addrModeAutoIndexing instr = 2 + nrCyclesOp op
+  | addrModeIndirect instr     = 1 + nrCyclesOp op
+  | otherwise                  =     nrCyclesOp op
+
+addrModeAutoIndexing (Instr op ind mp off _) =
+  ind == Just Indirect &&
+  mp  == Just ZeroPage &&
+  off >= Just (oct 10)
+  off <= Just (oct 17)
+addrModeIndirect (Instr op ind mp off _) =
+  ind == Just Indirect && (   mp /= Just ZeroPage
+                           || off < Just (oct 10)
+                           || off > Just (oct 17)
+                          )
+
+-- Interprete a numeric literal as octal
+oct = fst . head . readOct . filter isOctDigit . show :: Int -> Int
