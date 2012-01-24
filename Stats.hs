1256c0b869299f2cef2b08ff4693b369df3ab8bb
Thomas M. DuBuisson <Thomas.DuBuisson@gmail.com>
2012-01-24 13:52:03 -0800

    Make Stats.hs parse.

--- Stats.hs
+++ Stats.hs
@@ -1,15 +1,18 @@
 module Stats
   ( renderStats
   ) where
+import Parse
+import Types
+import qualified Data.Map as M
 
 renderStats :: Stats -> String
 renderStats (Stats cy inst) =
   let nrInst = sum (M.elems inst)
-  in "Total instructions: " ++ nrInst ++
-     "Total cylces:       " ++ cy ++
+  in "Total instructions: " ++ show nrInst ++
+     "Total cycles:       " ++ show cy ++
      "Breakdown:          " ++ renderAll (M.toList inst)
  where
   renderAll :: [(PDPOp,Integer)] -> String
-  renderall = unlines. map ("                 " ++) . map renderLn
+  renderAll = unlines. map ("                 " ++) . map renderLn
   renderLn :: (PDPOp,Integer) -> String
   renderLn (o,i) = show o ++ " " ++ show i
--- Types.hs
+++ Types.hs
@@ -4,6 +4,7 @@ module Types
     -- * Types
   , MachineState(..)
   , Purpose
+  , Stats(..)
     -- * Helpers
   , initialState, initialMemory) where
 
