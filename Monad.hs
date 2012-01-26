43339323e961ad0fe0a078b8af6bd77e47d53459
Thomas M. DuBuisson <Thomas.DuBuisson@gmail.com>
2012-01-26 13:45:16 -0800

    Clean up the monad, add proper instances, add helper functions.

--- Monad.hs
+++ Monad.hs
@@ -1,9 +1,18 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Monad 
-  ( evalPDP8
+  ( -- * Monad Execution
+    evalPDP8
   , logPDP8
   , execPDP8
   , runPDP8
   , PDP8
+    -- * Utilities
+  , logMem
+  , logBranch
+  , modMem
+  , getPC, setPC, modPC
+  , getL, setL, modL
+  , getSR, setSR, modSR
   ) where
 
 import Control.Monad.State
@@ -11,6 +20,34 @@ import Control.Monad.Writer
 import Types
 
 newtype PDP8 a = PDP8 { unPDP8 :: StateT MachineState (WriterT Branches (WriterT MemoryLog IO)) a }
+  deriving (MonadState MachineState, Monad, MonadWriter Branches)
+
+logBranch :: Addr -> Addr -> PDP8 ()
+logBranch src target = tell [(src,target)]
+
+logMem :: Purpose -> Addr -> PDP8 ()
+logMem p a = PDP8 $ lift $ lift (tell [(p,a)])
+
+modMem :: (Memory -> Memory) -> PDP8 ()
+modMem f = modify (\s -> s { mem = f (mem s)})
+
+getPC, getSR :: PDP8 Int12
+getPC   = gets pc
+getSR   = gets sr
+
+setPC, setSR :: Int12 -> PDP8 ()
+setPC x = get >>= \s -> put s { pc = x }
+setSR x = get >>= \s -> put s { sr = x }
+
+modPC, modSR :: (Int12 -> Int12) -> PDP8 ()
+modPC f = getPC >>= setPC . f
+modSR f = getSR >>= setSR . f
+
+getL   = gets lb
+setL x = get >>= \s -> put s { lb = x }
+
+modL :: (Int -> Int) -> PDP8 ()
+modL f = getL >>= setL . f
 
 evalPDP8 :: PDP8 a -> IO a
 evalPDP8 = liftM (\(m,b,s,a) -> a) . runPDP8
