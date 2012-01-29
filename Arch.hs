06afdd780534ce8ea7c5df3e4d46e561301e52ba
Thomas M. DuBuisson <Thomas.DuBuisson@gmail.com>
2012-01-29 12:37:42 -0800

    use boolean tests to count cycles.

--- Arch.hs
+++ Arch.hs
@@ -5,20 +5,25 @@ module Arch
 import Numeric (readOct)
 import Data.Char (isOctDigit)
 import Memory
+import Types
 
 -- FIXME put any constants here!  Constants such as the auto-increment values.
 
-nrCyclesOp :: PDPOp -> Int
+nrCyclesOp :: Integral a => PDPOp -> a
 nrCyclesOp o
-  | op >= 0 && <= 4 = 2
-  | op == 5         = 1
-  | op == 6         = 0
-  | otherwise       = 1
- where
- op = fromEnum o
+  | opIsMem o          = 2
+  | opIsIO o           = 0
+  | opIsMicro o        = 1
+  | otherwise          = 0 --Unknown ops
 
-nrCycles :: Instr -> Int
+nrCycles :: Integral a => Instr -> a
 nrCycles instr@(Instr op _ _ _ _)
   | addrModeAutoIndexing instr = 2 + nrCyclesOp op
   | addrModeIndirect instr     = 1 + nrCyclesOp op
   | otherwise                  =     nrCyclesOp op
+
+addrModeIndirect :: Instr -> Bool
+addrModeIndirect = undefined
+
+addrModeAutoIndexing :: Instr -> Bool
+addrModeAutoIndexing = undefined
