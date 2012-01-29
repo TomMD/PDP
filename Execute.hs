9292cb486f5ff93b53403d0b5e915aa1a8be46ac
Thomas M. DuBuisson <Thomas.DuBuisson@gmail.com>
2012-01-29 12:38:46 -0800

    Outline an execution framework.  Discard this at will.

--- Execute.hs
+++ Execute.hs
@@ -1,3 +1,43 @@
-module Execute where
+module Execute
+       ( step
+       ) where
+
+import Monad
+import Stats
+import Types
+import Memory
+
+step :: PDP8 ()
+step = do
+  p <- getPC
+  i <- fetch (Addr p)
+  incStats i
+  stepInstr i
+  modPC (+1) -- FIXME adding just 1, PC is in words I hope?
+
+stepInstr :: Instr -> PDP8 ()
+stepInstr i@(Instr (PDPOpMem mo)    _ _ _ _) = stepInstrMemOp i
+stepInstr i@(Instr (PDPOpIO ms) _ _ _ _)     = stepInstrIOOp i
+stepInstr i@(Instr (PDPOpMicro1 ms) _ _ _ _) = stepInstrMicro1 i
+stepInstr i@(Instr (PDPOpMicro2 ms) _ _ _ _) = stepInstrMicro2 i
+stepInstr i@(Instr (PDPOpMicro3 ms) _ _ _ _) = stepInstrMicro3 i
+stepInstr i@(Instr UnknownOp        _ _ _ _) = stepInstrUnknown i
 
 
+stepInstrMemOp :: Instr -> PDP8 ()
+stepInstrMemOp = undefined
+
+stepInstrIOOp :: Instr -> PDP8 ()
+stepInstrIOOp = undefined
+
+stepInstrMicro1 :: Instr -> PDP8 ()
+stepInstrMicro1 = undefined
+
+stepInstrMicro2 :: Instr -> PDP8 ()
+stepInstrMicro2 = undefined
+
+stepInstrMicro3 :: Instr -> PDP8 ()
+stepInstrMicro3 = undefined
+
+stepInstrUnknown   :: Instr -> PDP8 ()
+stepInstrUnknown _ = return () -- FIXME make a new log in the transformer stack and log this event?
