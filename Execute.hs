4bae51d775fbbbe29b31a96e147d15b006784836
J. Garrett Morris <jgmorris@cs.pdx.edu>
2012-02-09 14:28:58 -0800

    Fetch major stage, probably not right or anything.

--- Execute.hs
+++ Execute.hs
@@ -2,42 +2,92 @@ module Execute
        ( step
        ) where
 
+import Control.Monad
+import Data.Bits
+
 import Monad
 import Stats
 import Types
 import Memory
+import Util
 
-step :: PDP8 ()
-step = do
-  p <- getPC
-  i <- fetch (Addr p)
-  incStats i
-  stepInstr i
-  modPC (+1) -- FIXME adding just 1, PC is in words I hope?
+step = fetch
 
-stepInstr :: Instr -> PDP8 ()
-stepInstr i@(Instr (PDPOpMem _)    _ _ _ _) = stepInstrMemOp i
-stepInstr i@(Instr (PDPOpIO _)     _ _ _ _) = stepInstrIOOp i
-stepInstr i@(Instr (PDPOpMicro1 _) _ _ _ _) = stepInstrMicro1 i
-stepInstr i@(Instr (PDPOpMicro2 _) _ _ _ _) = stepInstrMicro2 i
-stepInstr i@(Instr (PDPOpMicro3 _) _ _ _ _) = stepInstrMicro3 i
-stepInstr i@(Instr UnknownOp       _ _ _ _) = stepInstrUnknown i
+-- Major states
+fetch :: PDP8 ()
+defer, execute :: Instr -> PDP8 ()
 
+fetch = do modPC (1+)
+           iaddr <- getCPMA
+           i <- fetchInstruction (Addr iaddr)
+           case i of
+             UNK -> finish -- TODO: log
+             OP1 cla micros logical -> doOp1 cla micros logical
+             OP2 cla invertAndUnion skips micros -> doOp2 cla invertAndUnion skips micros
+             OP3 cla micros -> doOp3 cla micros
+             IOT ioop -> doIO ioop
+             JMP Direct page offset -> doDirectJump page offset
+             _ -> do when (page i == ZeroPage) (modCPMA (.&. oct 377))
+                     case indirection i of
+                       Indirect -> execute i
+                       Direct -> defer i
+    where doOp1 cla micros logical =
+              do when cla (setAC 0)
+                 when (CLL `elem` micros) (setL 0)
+                 when (CMA `elem` micros) (modAC complement)
+                 when (CML `elem` micros) (modL complement)
+                 when (IAC `elem` micros) (modAC (+1))
+                 case logical of
+                   Nothing -> return ()
+                   Just RAR -> modLAC rotR
+                   Just RTR -> modLAC (rotR . rotR)
+                   Just RAL -> modLAC rotL
+                   Just RTL -> modLAC (rotL . rotL)
+                   Just BSW -> modAC (\a -> let high = (a .&. oct 7700) `shiftR` 6
+                                                low  = (a .&. oct 0077) `shiftL` 6
+                                            in low .|. high)
+                 finish
+              where rotL, rotR :: (Int,Int12) -> (Int,Int12)
+                    rotL (l, a) = (chiNeq (a .&. 4000) 0, a `shiftL` 1 .|. fromIntegral l)
+                    rotR (l, a) = (chiNeq (a .&. 1) 0, fromIntegral (l `shiftL` 11) .|. a `shiftR` 1)
+                    chiNeq x y | x /= y    = 1
+                               | otherwise = 0
 
-stepInstrMemOp :: Instr -> PDP8 ()
-stepInstrMemOp = undefined
+          doOp2 cla invertAndUnion skips micros =
+              do logic invertAndUnion skips
+                 when cla (setAC 0)
+                 when (OSR `elem` micros) (modAC . (.|.) =<< getSR)
+                 when (HLT `elem` micros) halt
+                 finish
+              where logic True []     = skipIf True
+                    logic False skips = skipIf . or =<< mapM check skips
+                    logic True skips  = skipIf . and . map not =<< mapM check skips
 
-stepInstrIOOp :: Instr -> PDP8 ()
-stepInstrIOOp = undefined
+                    check SMA = (< 0) `fmap` getAC
+                    check SZA = (== 0) `fmap` getAC
+                    check SNL = (/= 0) `fmap` getL
 
-stepInstrMicro1 :: Instr -> PDP8 ()
-stepInstrMicro1 = undefined
+                    skipIf True  = modPC (1 +)
+                    skipIf False = return ()
 
-stepInstrMicro2 :: Instr -> PDP8 ()
-stepInstrMicro2 = undefined
+          doOp3 cla micros =
+              do when cla (setAC 0)
+                 when (MQL `elem` micros) (do setMQ =<< getAC
+                                              setAC 0)
+                 when (MQA `elem` micros) (modAC . (.|.) =<< getMQ)
+                 finish
 
-stepInstrMicro3 :: Instr -> PDP8 ()
-stepInstrMicro3 = undefined
+          doIO _ = finish
 
-stepInstrUnknown   :: Instr -> PDP8 ()
-stepInstrUnknown _ = return () -- FIXME make a new log in the transformer stack and log this event?
+          doDirectJump ZeroPage offset =
+              do setPC offset
+                 finish
+          doDirectJump CurrentPage offset =
+              do cpma <- getCPMA
+                 setPC ((cpma .&. oct 7600) .|. offset)
+                 finish
+
+          finish = setCPMA =<< getPC
+
+defer = undefined
+execute = undefined
